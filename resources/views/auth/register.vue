d4536e935a0a9ff0f289568302a0bad969a72636
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-06-21 13:35:37 +0100

    template for html of reg

--- resources/views/auth/register.vue
+++ resources/views/auth/register.vue
@@ -1,12 +1,24 @@
-<template>
+<template xmlns:v="http://www.w3.org/1999/xhtml">
     <div class="registration-container">
         <h2 class="">Register</h2>
-      <div class="name-form">
-          <
+
+        <div class="form-group">
+            <label>Name</label>
+            <input v-model="form.name" type="text" required />
       </div>
+
+        <div class="form-group">
+            <label>Email</label>
+            <input v-model="form.email" type="text" required />
+        </div>
+        <div class="form-group">
+            <label>Password</label>
+            <input v-model="form.password" type="password" required />
+        </div>
     </div>
 </template>
 <script>
+ import { ref } from 'vue';
 
 </script>
 <style scoped>
@@ -17,11 +29,10 @@
     max-width: 400px;
     background: #fff;
     border-radius: 1px;
+    padding: 2rem;
 }
-.name-form {
-padding: 0 15px;
-    display: flex;
-
+.form-group {
+    margin-bottom: 1rem;
 }
 .h2 {
     color: #0a0a0a;
