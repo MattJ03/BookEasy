efc55011270fc322dede14504ebee91940b53136
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-06-21 18:10:49 +0100

    vue router set up path and component for register

---
+++ resources/js/router/index.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import Register from '../views/Register.vue';
+import Login from '../views/Login.vue';
+
+const routes = [
+    {path: '/register', component: auth.register}
+    ];
