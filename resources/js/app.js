cefa2211ca031aea63acb8edc875e4c06212b751
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-06-21 20:56:41 +0100

    added spa route

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import router from './router/index.js';
+import App from './App.vue'
+
+const app = createApp(App);
+app.use(router);
+app.use(createPinia());
+app.mount('#app');
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>BookEasy</title>
+    @vite('resources/js/app.js')
+</head>
+<body>
 <div id="app"></div>
-@vite('resources/js/')
+</body>
+</html>
