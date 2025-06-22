f361bc8f61b76398f9d57536b2071934121a4b54
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-06-22 07:58:39 +0100

    upadated vite config and added new imports

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import { createRouter } from 'vue-router';
 import { createWebHistory } from 'vue-router';
-import Register from '../../views/auth/Register.vue';
-import Login from '../../views/auth/Login.vue'
+import Register from '../views/auth/Register.vue';
+
 
 const routes = [
     { path: '/register', component: Register },
