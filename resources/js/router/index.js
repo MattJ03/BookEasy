95cbce44c1b52d45d7803ce138f2e3793382688f
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-07-02 00:01:06 +0100

    added new route to router file and created home page

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,12 +3,12 @@ import { createRouter } from 'vue-router';
 import { createWebHistory } from 'vue-router';
 import Register from '../views/auth/Register.vue';
 import Login from '../views/auth/Login.vue';
-
+import Home from '../views/listings/Home.vue';
 
 const routes = [
     { path: '/register', component: Register },
     { path: '/login', component: Login },
-    { path: '/listing/index', component: Listing}
+    { path: '/home', component: Home },
     ];
 
 const router = createRouter({
