8eab4f04aa7e661ed2d3e8ebf25e65f3212a3a60
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-07-03 22:50:35 +0100

    getting data from listing api

--- resources/js/views/listings/Home.vue
+++ resources/js/views/listings/Home.vue
@@ -1,10 +1,41 @@
 <template>
+<div class="listings">
+    <p v-if=""
 
+</div>
 </template>
-<script>
+<script setup>
+import { ref } from 'vue';
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import ListingCard from "../../components/ListingCard.vue";
+import { onMounted } from 'vue';
+import axios from 'axios';
 
-</script setup>
+const router = useRouter();
+const listings = reactive({});
+const loading = ref(true);
+const error = ref('');
+
+onMounted(async () => {
+    try {
+        const response = await axios.get('/api/listings/index');
+        listing.value = response.data;
+    } catch(error) {
+        error.value = error.response.data.errors;
+    } finally {
+        loading.value = false;
+    }
+});
+</script>
 
 <style>
+.listings {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+
 
 </style>
