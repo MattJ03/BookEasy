3419287585c965ee862d5b629f1156431713e988
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-07-04 23:58:16 +0100

    refactoring methods in listing controller

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -12,5 +12,30 @@ use App\Services\ListingService;
 
 class ListingController extends Controller
 {
+    public function index() {
+        $listings = Listing::where('user_id', '!=', Auth::id())
+            ->paginate(25);
+        return response()->json($listings);
+
+    }
+    public function store(Request $request, ListingService $listingService) {
+        $validatedData = $request->validate([
+            'name' => 'required|max:30',
+            'price' => 'required|numeric',
+            'availability' => 'required|boolean',
+            'user_id' => 'required',
+            'image_path' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+
+        ]);
+
+       $listing = $listingService->store($validatedData);
+        return response()->json($listing, 201);
+    }
+
+   public function show($id) {
+        $listing = Listing::find($id);
+        if(!$listing) {}
+        return response()->json('Message', 'No listing found');
+   }
 
 }
