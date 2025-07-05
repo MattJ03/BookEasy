5e316dc4ed514fb668dc7e33d03da9cde1ae4366
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-07-05 11:31:25 +0100

    updated routes

---
+++ resources/js/views/listings/Create.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="create-form">
+        <h2>Create Listing</h2>
+        <form @submit.prevent=""
+    </div>
+
+</template>
+<script setup>
+import { ref } from 'vue';
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import axios from 'axios';
+
+const router = useRouter();
+const error = ref('');
+const form = reactive({
+    name: "",
+    price: "",
+    availability: true,
+    image: ""
+});
+
+const submitForm = (async) => {
+    try {
+        await axios.post('/api/listings/store');
+    }
+}
+</script>
+<style>
+.create-form {
+    width: 400px;
+    height: 500px;
+    margin: 100px auto;
+    justify-content: center;
+    align-items: center;
+}
+</style>
+
