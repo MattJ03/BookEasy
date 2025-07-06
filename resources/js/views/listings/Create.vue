90899c29ca01d3e9649b6d96f9353963a15c77ed
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-07-06 10:04:09 +0100

    changes to borders in input fields

--- resources/js/views/listings/Create.vue
+++ resources/js/views/listings/Create.vue
@@ -1,7 +1,24 @@
 <template>
     <div class="create-form">
-        <h2>Create Listing</h2>
-        <form @submit.prevent=""
+        <h2 class="h2">Create Listing</h2>
+        <form @submit.prevent="submitForm">
+            <div class="form-group">
+              <label>Image of property </label>
+                <input type="file" accept="image/*" class="image-listing" required>
+            </div>
+            <div class="form-group">
+                <label>Name of property </label>
+                <input v-model="form.name" type="text" required>
+            </div>
+            <div class="form-group">
+                <label>Price </label>
+                <input v-model="form.price" type="number" required>
+            </div>
+            <div class="form-group">
+                <label>Available to Book? </label>
+                <input v-model="form.availability" type="checkbox" value="yes">
+            </div>
+        </form>
     </div>
 
 </template>
@@ -12,7 +29,7 @@ import { useRouter } from 'vue-router';
 import axios from 'axios';
 
 const router = useRouter();
-const error = ref('');
+const error = ref(null);
 const form = reactive({
     name: "",
     price: "",
@@ -24,16 +41,36 @@ const submitForm = async () => {
     try {
         await axios.post('/api/listings/store', form);
         await router.push('/home');
+    } catch(error) {
+        error.value = error.response.data.errors;
     }
 }
 </script>
 <style>
 .create-form {
-    width: 400px;
-    height: 500px;
+    width: 800px;
+    height: 800px;
     margin: 100px auto;
     justify-content: center;
     align-items: center;
+    background: #a7151d;
+    border-radius: 12px;
+    padding: 2rem;
+}
+.image-listing {
+    margin:  auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+}
+.form-group {
+    margin-bottom: 3rem;
+    border-radius: 10px;
+    width: 50%;
+    background: #edf2f7;
+}
+.h2 {
+    justify-content: center;
 }
 </style>
 
